a21f282d76639a7fa2f97dca815ea3563234568a
Lukas Carvajal <lukascarvajal@gmail.com>
2024-05-23 18:51:55 +0200

    Remove notebooks from tracking

--- real-open-data/notebooks/constants.py
+++
@@ -1,3 +0,0 @@
-DATA_SETS_BASE_URL = "https://www.data.gv.at/katalog/api/3/action/package_search"
-DATA_SETS_DIRECTORY_PATH = './data-sets/'
-DATA_SETS_META_FILE_PATH = f'{DATA_SETS_DIRECTORY_PATH}data_sets_meta.csv'
