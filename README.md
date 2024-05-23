35cecbedcf16104ed978b3d8d832c0f959479ff5
Lukas Carvajal <lukascarvajal@gmail.com>
2024-05-23 15:59:56 +0200

    Configure PostgreSQL

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Real Open Data
 
 A project aimed at making open data in Vienna more accessible.
+
+ ## Tools
+ - Django - python manage.py runserver
+ - PostgreSQL - brew services start postgresql@15
--- real-open-data/settings.py
+++ real-open-data/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 
+env = environ.Env()
+environ.Env.read_env()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mkb45yk#10lds*ibnqj-u0-=$)inx)ql2!n5y3m+j7z&p+#p+$'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -75,8 +78,12 @@ WSGI_APPLICATION = 'real-open-data.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env("DB_NAME"), 
+        'USER': env("DB_USER"),
+        'PASSWORD': env("DB_PASSWORD"),
+        'HOST': env("DB_HOST"), 
+        'PORT': env("DB_PORT"),
     }
 }
 
