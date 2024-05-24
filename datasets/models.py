aba4cbaae677f87bfaf2a089b907f2b0c7a2959e
Lukas Carvajal <lukascarvajal@gmail.com>
2024-05-24 11:16:46 +0200

    Add comment for running migrations to help me remember what to do

--- datasets/models.py
+++ datasets/models.py
@@ -3,7 +3,8 @@ from django.db import models
 # Create your models here.
 
 # Run `python manage.py makemigrations` after making changes
+# Then run `python manage.py migrate`
 
-class Teacher(models.Model):
+class Dataset(models.Model):
     name = models.CharField(max_length=80)
-    age = models.IntegerField()
+    file_path = models.CharField(max_length=80)
