f87b46dbfefb6fb4a0736e318e738c78ff6f4510
Lukas Carvajal <lukascarvajal@gmail.com>
2024-05-24 16:34:11 +0200

    Set up Dataset model along with admin

--- datasets/models.py
+++ datasets/models.py
@@ -2,9 +2,14 @@ from django.db import models
 
 # Create your models here.
 
+# Check if there are problems with ` python manage.py check`
 # Run `python manage.py makemigrations` after making changes
 # Then run `python manage.py migrate`
 
 class Dataset(models.Model):
-    name = models.CharField(max_length=80)
+    title = models.CharField(max_length=80)
     file_path = models.CharField(max_length=80)
+
+    # Displays the title when the dataset is displayed in the console or admin
+    def __str__(self):
+        return self.title
