2868480381ccf12aec92981d4295de9262b7edd4
Lukas Carvajal <lukascarvajal@gmail.com>
2024-05-27 11:47:55 +0200

    Configure urls for datasets views

--- datasets/urls.py
+++ datasets/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    # ex: /datasets/5/
+    path("<int:dataset_id>/", views.detail, name="detail"),
+    # ex: /datasets/5/analysis/
+    path("<int:dataset_id>/analysis/", views.analysis, name="analysis"),
 ]
--- datasets/views.py
+++ datasets/views.py
@@ -4,3 +4,11 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the datasets index.")
+
+def detail(request, dataset_id):
+    response = "You're looking at data set %s."
+    return HttpResponse(response % dataset_id)
+
+def analysis(request, dataset_id):
+    response = "You're looking at the analysis path for data set %s."
+    return HttpResponse(response % dataset_id)
