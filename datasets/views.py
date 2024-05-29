fab8e26c08b47abe379984f20aff1e94cab94962
Lukas Carvajal <lukascarvajal@gmail.com>
2024-05-29 23:19:26 +0200

    Hook up select element to switching districts

--- datasets/views.py
+++ datasets/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse
-from django.http import Http404
+from django.conf import settings
 
+import json
 from .models import Dataset
+import os
+import pandas as pd
 
 def index(request):
     datasets_list = Dataset.objects.order_by("title")
@@ -11,7 +13,40 @@ def index(request):
 
 def detail(request, dataset_id):
     dataset = get_object_or_404(Dataset, pk=dataset_id)
-    return render(request, 'datasets/detail.html', { 'dataset': dataset })
+
+    # Get query parameters
+    file_path = os.path.join(settings.STATIC_ROOT, dataset.file_path)
+    df = pd.read_csv(file_path)
+    x_column = request.GET.get('x')
+    y_column = df.columns[2]
+    filter_column = request.GET.get('filter')
+    current_filter_option = int(request.GET.get('filter_value'))
+
+    print(df[filter_column].unique())
+    print(current_filter_option)
+
+    # JSON data for chart generation
+    filtered_df = df[df[filter_column] == current_filter_option]
+    labels = filtered_df[x_column].tolist()
+    data = filtered_df[y_column].tolist()
+    chart_data = {
+        'labels': labels,
+        'datasets': [{
+            'label': 'Dataset Label',  # You can customize this label
+            'data': data
+        }]
+    }
+    json_data = json.dumps(chart_data)
+
+    context = {
+        'dataset': dataset,
+        'filter_title': filter_column,
+        'filter_options': df[filter_column].unique(),
+        'current_filter_option': current_filter_option,
+        'json_data': json_data
+    }
+
+    return render(request, 'datasets/detail.html', context)
 
 def analysis(request, dataset_id):
     dataset = get_object_or_404(Dataset, pk=dataset_id)
