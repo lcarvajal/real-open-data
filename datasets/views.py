759bbc29b8cb82df63d91f8bd336b675e022805e
Lukas Carvajal <lukascarvajal@gmail.com>
2024-05-31 08:19:25 +0200

    Replace chart generation through parameters with chart table from database

--- datasets/views.py
+++ datasets/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import get_object_or_404, render
 from django.conf import settings
 
 import json
-from .models import Dataset
+from .models import Dataset, Chart
 import os
 import pandas as pd
 
@@ -13,26 +13,24 @@ def index(request):
 
 def detail(request, dataset_id):
     dataset = get_object_or_404(Dataset, pk=dataset_id)
+    charts = dataset.chart_set.all()
+    selected_chart = charts[0]
 
-    # Get query parameters
     file_path = os.path.join(settings.STATIC_ROOT, dataset.file_path)
     df = pd.read_csv(file_path)
-    x_column = request.GET.get('x')
-    y_column = df.columns[2]
-    filter_column = request.GET.get('filter')
-    current_filter_option = int(request.GET.get('filter_value'))
-
-    print(df[filter_column].unique())
-    print(current_filter_option)
 
     # JSON data for chart generation
-    filtered_df = df[df[filter_column] == current_filter_option]
-    labels = filtered_df[x_column].tolist()
-    data = filtered_df[y_column].tolist()
+    current_filter_option = request.GET.get('filter_value')
+    if current_filter_option is None:
+        current_filter_option = 91700
+
+    current_filter_option = int(current_filter_option)
+    filtered_df = df[df[selected_chart.filter_column] == current_filter_option]
+    labels = filtered_df[selected_chart.x_column].tolist()
+    data = filtered_df[selected_chart.y_column].tolist()
     chart_data = {
         'labels': labels,
         'datasets': [{
-            'label': 'Dataset Label',  # You can customize this label
             'data': data
         }]
     }
@@ -40,10 +38,10 @@ def detail(request, dataset_id):
 
     context = {
         'dataset': dataset,
-        'filter_title': filter_column.replace("_", " ").lower(),
-        'filter_options': df[filter_column].unique(),
-        'x_title': x_column.replace("_", " ").lower(),
-        'y_title': y_column.replace("_", " ").lower(),
+        'filter_title': selected_chart.filter_column.replace("_", " ").lower(),
+        'filter_options': df[selected_chart.filter_column].unique(),
+        'x_title': selected_chart.x_column.replace("_", " ").lower(),
+        'y_title': selected_chart.y_column.replace("_", " ").lower(),
         'current_filter_option': current_filter_option,
         'json_data': json_data
     }
