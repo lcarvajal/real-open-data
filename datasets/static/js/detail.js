8659b1bf6f7468f79886a834e85220e282719331
Lukas Carvajal <lukascarvajal@gmail.com>
2024-06-03 13:45:41 +0200

    Move logic for setting onchange value from detail.html to detail.js

--- datasets/static/js/detail.js
+++ datasets/static/js/detail.js
@@ -1,42 +1,45 @@
-function makeChart() {
-    let chart = JSON.parse(document.getElementById('chart-data').textContent);
-
-    // Using Chart.js to create a chart
-    new Chart('chart', {
-        type: chart.type,
-        options: {
-            maintainAspectRatio: false,
-            legend: {
-                display: false
+$(document).ready(function(){
+    function makeChart() {
+        let chart = JSON.parse(document.getElementById('chart-data').textContent);
+    
+        // Using Chart.js to create a chart
+        new Chart('chart', {
+            type: chart.type,
+            options: {
+                maintainAspectRatio: false,
+                legend: {
+                    display: false
+                },
+                scales: {
+                    yAxes: [{
+                        scaleLabel: {
+                        display: true,
+                        labelString: chart.y_title,
+                        }
+                    }],
+                    xAxes: [{
+                        scaleLabel: {
+                        display: true,
+                        labelString: chart.x_title,
+                        }
+                    }]
+                }
             },
-            scales: {
-                yAxes: [{
-                    scaleLabel: {
-                    display: true,
-                    labelString: chart.y_title,
-                    }
-                }],
-                xAxes: [{
-                    scaleLabel: {
-                    display: true,
-                    labelString: chart.x_title,
-                    }
+            data: {
+                labels: chart.x_labels,
+                datasets: [{
+                    data: chart.y_values
                 }]
             }
-        },
-        data: {
-            labels: chart.x_labels,
-            datasets: [{
-                data: chart.y_values
-            }]
-        }
-    });
-}
-
-function updateChart(val) {
-    let filterOption = $('#filter').find(":selected").val();
-    let url = $('#filter').data('url');
-    window.location.replace(url.replace('temp', filterOption))
-}
-
-makeChart();
+        });
+    }
+    
+    function changeFilterHandler(event) {
+        let filterOption = $(event.target).val();
+        let url = $('#filter').data('url');
+        window.location.replace(url.replace('temp', filterOption))
+    }
+    
+    $('#filter').on('change', changeFilterHandler);
+    makeChart();
+});
