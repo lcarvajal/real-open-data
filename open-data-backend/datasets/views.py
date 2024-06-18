8d306da0adfb30771def13223e6a3c5c1cebffdc
Lukas Carvajal <lukascarvajal@gmail.com>
2024-06-19 00:33:22 +0200

    Configure chartjs with react

--- open-data-backend/datasets/views.py
+++ open-data-backend/datasets/views.py
@@ -22,7 +22,42 @@ def detail(request, dataset_id):
         first_filter_value = selected_chart.get_first_filter_value()
         filter_value = first_filter_value.astype(str)
 
+    chart_context = selected_chart.get_context(filter_value)
+
+    chart_options = {
+                'maintainAspectRatio': False,
+                'legend': {
+                    'display': False
+                },
+                'scales': {
+                    'x': {
+                        'display': True,
+                        'title': {
+                        'display': True,
+                        'text': chart_context['y_title'],
+                        'padding': { 'top': 20, 'left': 0, 'right': 0, 'bottom': 0 },
+                        }
+                    },
+                    'y': {
+                        'display': True,
+                        'title': {
+                        'display': True,
+                        'text': chart_context['x_title'],
+                        'padding': { 'top': 30, 'left': 0, 'right': 0, 'bottom': 0 },
+                        }
+                    }
+                }
+            }
+    chart_data = {
+                'labels': chart_context['x_labels'],
+                'datasets': [{
+                    'data': chart_context['y_values']
+                }]
+            }
+
     return Response({
         "dataset": serializer.data,
-        "chart": selected_chart.get_context(filter_value)
+        "chart": chart_context,
+        "chart_options": chart_options,
+        "chart_data": chart_data
     })
--- open-data-frontend/src/components/DatasetDetail.js
+++ open-data-frontend/src/components/DatasetDetail.js
@@ -1,16 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
+import { Line } from 'react-chartjs-2';
+import Chart from 'chart.js/auto';
 
 export default function DatasetDetail(props) {
-  const [dataset, setDataset] = useState({});
+  const [responseData, setResponseData] = useState({});
   const { id } = useParams();
 
   useEffect(() => {
     axios.get('http://localhost:8000/datasets/' + id)
       .then(response => {
         console.log(response.data);
-        setDataset(response.data.dataset);
+        setResponseData(response.data);
       })
       .catch(error => {
         console.log(error);
@@ -20,12 +22,17 @@ export default function DatasetDetail(props) {
   return (
     <div>
       {
-        dataset ? (
-          <p>{dataset.title}</p>
+        responseData && responseData.dataset ? (
+          <>
+            <p>{responseData.dataset.title}</p>
+            <div>
+              <Line data={responseData.chart_data} options={responseData.chart_options} />
+            </div>
+          </>
         ) : (
           <p>Dataset loading</p>
         )
       }
-    </div>
+    </div >
   );
 }
